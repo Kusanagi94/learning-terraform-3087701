2190b1dab867808230758b8d440d2d1d95344269
Kusanagi94 <39561537+Kusanagi94@users.noreply.github.com>
2024-05-15 16:52:16 -0400

    Update alb module version

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -47,7 +47,7 @@ module "autoscaling" {
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  version = "9.9.0"
 
   name = "${var.environment.name}-blog-alb"
 
